c05b665aa4374a7fb5d37be097b9b6a72a1132a8
masterKiE <prawit.na59@snru.ac.th>
2018-02-20 12:01:31 +0700

    Add Freagment Success

--- app/src/main/java/kie/snru/ac/th/emercall/MainActivity.java
+++ app/src/main/java/kie/snru/ac/th/emercall/MainActivity.java
@@ -3,11 +3,24 @@ package kie.snru.ac.th.emercall;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import kie.snru.ac.th.emercall.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//     add Fragment
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment()).commit();
+
+        }
+
+
+    } //main Method
+
+}  // main Class
